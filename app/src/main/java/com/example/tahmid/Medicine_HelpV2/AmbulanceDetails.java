7edc391cd3e5e9ef64d6d206039bcefb14d23218
Tahmid46 <ashiqur.rahman1023@gmail.com>
2019-12-18 19:05:39 +0600

    Update_18_Dec

---
+++ app/src/main/java/com/example/tahmid/Medicine_HelpV2/AmbulanceDetails.java
@@ -0,0 +1,13 @@
+package com.example.tahmid.Medicine_HelpV2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AmbulanceDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ambulance_details);
+    }
+}
